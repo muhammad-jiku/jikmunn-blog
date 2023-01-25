eeace84d7b0f61336570464a1e571fcc0dbf8403
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-25 14:30:02 +0600

    add blog file modified

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -7,6 +7,12 @@ const Navbar = () => {
       <ul className="flex justify-around">
         <li className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700">
           <Link href={`/`}> Home</Link>
+        </li>{' '}
+        <li className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700">
+          <Link href={`/blogs/`}> Blogs</Link>
+        </li>{' '}
+        <li className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700">
+          <Link href={`/blogs/add/`}> Add Blog</Link>
         </li>
       </ul>
     </nav>
