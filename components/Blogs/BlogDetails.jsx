b88b1099a24a9c0797f6afd0781f3d4eff371feb
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-25 20:34:01 +0600

    unnecessary data removed

--- components/Blogs/BlogDetails.jsx
+++ components/Blogs/BlogDetails.jsx
@@ -12,11 +12,6 @@ const BlogDetails = ({ blog }) => {
         <div className="mb-2 text-xl font-bold"> {blog?.title} </div>
         <p className="text-base text-gray-700"> {blog?.desc} </p>
       </div>
-      <div className="text-center">
-        <button className="px-4 py-2 font-semibold text-blue-700 bg-transparent border border-blue-500 rounded hover:bg-blue-500 hover:text-white hover:border-transparent">
-          Read More..
-        </button>
-      </div>
     </div>
   );
 };
