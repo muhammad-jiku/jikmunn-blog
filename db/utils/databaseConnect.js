0ebac502f7e64700423a8f385d337d661addf361
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-26 01:12:48 +0600

    db connect

---
+++ db/utils/databaseConnect.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASS}@cluster0.41e9axg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const databaseConnect = () => {
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connected!!');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = databaseConnect;
