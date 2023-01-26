bd757c586a22a7a37f58b38aca503e6a4389a27c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-26 14:37:46 +0600

    blogs pages

---
+++ pages/blogs/index.js
@@ -0,0 +1,23 @@
+import Blogs from '@/components/Blogs/Blogs';
+import React from 'react';
+
+const BlogsPage = ({ blogs }) => {
+  return (
+    <div>
+      {blogs?.map((blog, idx) => (
+        <Blogs key={idx} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default BlogsPage;
+
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:3000/api/blogs');
+  const responseData = await res.json();
+  const blogs = await responseData?.data;
+  return {
+    props: { blogs },
+  };
+};
