2dd42a2dcf7e94df2f581a1c2c8ce8170151aa2c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-26 14:27:42 +0600

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Navbar />
         {children}
       </body>
